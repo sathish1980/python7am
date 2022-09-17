a76a15e6366740cb18fac7a619493e28aee1c4b8
sathish1980 <kumar.sathish189@gmail.com>
2022-09-17 09:15:04 +0530

    selenium file

--- BasicofSelenium/CreateFirstTestcase.py
+++ BasicofSelenium/CreateFirstTestcase.py
@@ -9,20 +9,21 @@ class firstclass():
 
     def firsttestcase(self, web=None):
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=web)
-        self.driver.get("https://www.google.com/")
         self.driver.maximize_window()
+        #self.driver.minimize_window()
+        #self.driver.set_window_size(200, 600)
+        self.driver.get("https://www.google.com")
         self.driver.get("https://www.facebook.com/")
         self.driver.back()
-        #self.driver.set_window_size(200,600)
         time.sleep(2)
         self.driver.forward()
         time.sleep(2)
         self.driver.refresh()
         time.sleep(2)
 
-
-        #self.driver.close()
+        self.driver.close()
         self.driver.quit()
+
 
         #driver = webdriver.Chrome("D:\Software\chromedriver_win32\chromedriver.exe", options=options)
 bc= firstclass()
--- BasicofSelenium/Facebooklogin.py
+++ BasicofSelenium/Facebooklogin.py
@@ -10,23 +10,26 @@ class facbooklogin():
     def login(self, web=None):
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=web)
         self.driver.get("https://www.facebook.com/")
-        #username=self.driver.find_element(by=By.ID, value="email")
-        #username.send_keys("kumar.sathish189@gmail.com")
-        """username = self.driver.find_element(by=By.NAME, value="email") # webElement
-        username.send_keys("kumar.sathish189@gmail.com") #actions
-        username.clear()
-        username.send_keys("another value")"""
-        self.driver.find_element(by=By.CLASS_NAME, value="inputtext _55r1 _6luy").send_keys("")
-        self.driver.find_element(by=By.CLASS_NAME, value="inputtext _55r1 _6luy").clear()
-        self.driver.find_element(by=By.CLASS_NAME, value="inputtext _55r1 _6luy").send_keys("")
-        #or
-        username = self.driver.find_element(by=By.CLASS_NAME, value="inputtext _55r1 _6luy")  # webElement
-        username.send_keys("kumar.sathish189@gmail.com")  # actions
-        username.clear()
-        username.send_keys("another value")
-
-
+        self.driver.maximize_window()
+        email=self.driver.find_element(by=By.ID, value="email")
+        email.send_keys("kumar.sathish189@gmail.com124")
         time.sleep(2)
+        email.clear()
+        email.send_keys("nextvalue")
+        password = self.driver.find_element(by=By.NAME, value="pass")
+        password.send_keys("242454545")
+        time.sleep(2)
+        password.clear()
+        password.send_keys("nextvalue")
+        #password = self.driver.find_element(by=By.CLASS_NAME, value="_6lux")
+        print(self.driver.page_source)
+        self.driver.find_element(by=By.LINK_TEXT,value="Forgotten password?").click()
+        time.sleep(2)
+        """self.driver.find_element(by=By.PARTIAL_LINK_TEXT, value="ount?").click()"""
+
+        #self.driver.find_element(by=By.CSS_SELECTOR, value="input#identify_email").send_keys("sample")
+        self.driver.find_element(by=By.CSS_SELECTOR, value="input[placeholder = 'Email address or mobile number']").send_keys("sample")
+
 
 obj=facbooklogin()
 obj.login()
